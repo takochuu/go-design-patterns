5d2f36b6f0415bb11fc6f66364a9366efa63d179
takochuu <takochuu@gmail.com>
2017-09-11 23:22:02 +0900

    Add iterator

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/takochuu/go-design-patterns/composite"
+	"github.com/takochuu/go-design-patterns/iterator"
 )
 
 func main() {
+	// composite
 	list := composite.Newlist()
 	list.Add(composite.NewProductA())
 	list.Add(composite.NewProductB())
 	fmt.Println(list.GetAmount())
+
+	// iterator
+	bookshelf := iterator.BookShelf{}
+	bookshelf.Add(&iterator.Book{Name: "Hoge"})
+	bookshelf.Add(&iterator.Book{Name: "Fuga"})
+	bookshelf.Add(&iterator.Book{Name: "Piyo"})
+	i := bookshelf.Iterator()
+	for i.HasNext() {
+		book := i.Next()
+		fmt.Println(book.Name)
+	}
 }
